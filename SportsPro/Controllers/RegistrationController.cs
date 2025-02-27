3e8d0e8974ce9b916899d1515952a7303adb210d
SxrEntity <eddiejamesford750@gmail.com>
2025-02-27 04:37:54 -0500

    first commit

---
+++ SportsPro/Controllers/RegistrationController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsPro.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        //Index method and current home page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+    }
+}
