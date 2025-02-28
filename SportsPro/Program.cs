56719f2321755ae9b9a7f9180e0bd2d24b4ada39
SxrEntity <eddiejamesford750@gmail.com>
2025-02-28 00:16:46 -0500

    Turned off url lowercasing to fix countries page

--- SportsPro/Program.cs
+++ SportsPro/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRouting(options => {
-    options.LowercaseUrls = true;
+    // options.LowercaseUrls = true;
     options.AppendTrailingSlash = true;
 });
 
